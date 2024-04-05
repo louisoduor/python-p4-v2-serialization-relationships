8d228a56471e266086ada9465e9cc973cbab1a66
louisoduor <louisodhiambo20@gmail.com>
2024-04-05 09:53:07 +0000

    Completed Assignment

--- server/testing/models_test.py
+++ server/testing/models_test.py
@@ -1,31 +1,29 @@
-from models import *
-
-
-class TestAnimal:
-    '''Class Animal in models.py'''
-
-    def test_converts_to_dict(self):
-        '''can convert Animal objects to dictionaries.'''
-        a = Animal()
-        assert a.to_dict()
-        assert isinstance(a.to_dict(), dict)
-
-
-class TestEnclosure:
-    '''Class Enclosure in models.py'''
-
-    def test_converts_to_dict(self):
-        '''can convert Enclosure objects to dictionaries.'''
-        e = Enclosure()
-        assert e.to_dict()
-        assert isinstance(e.to_dict(), dict)
-
+from models import Zookeeper, Enclosure, Animal
 
 class TestZookeeper:
-    '''Class Zookeeper in models.py'''
-
     def test_converts_to_dict(self):
-        '''can convert Zookeeper objects to dictionaries.'''
-        z = Zookeeper()
-        assert z.to_dict()
-        assert isinstance(z.to_dict(), dict)
+        zk = Zookeeper(name='John', birthday='1990-01-01')
+        zk_dict = zk.to_dict()
+        assert isinstance(zk_dict, dict)
+        assert 'name' in zk_dict
+        assert 'birthday' in zk_dict
+
+class TestEnclosure:
+    def test_converts_to_dict(self):
+        enc = Enclosure(environment='Savannah', open_to_visitors=True)
+        enc_dict = enc.to_dict()
+        assert isinstance(enc_dict, dict)
+        assert 'environment' in enc_dict
+        assert 'open_to_visitors' in enc_dict
+
+class TestAnimal:
+    def test_converts_to_dict(self):
+        zookeeper = Zookeeper(name='John', birthday='1990-01-01')
+        enclosure = Enclosure(environment='Savannah', open_to_visitors=True)
+        animal = Animal(name='Lion', species='Big Cat', zookeeper=zookeeper, enclosure=enclosure)
+        animal_dict = animal.to_dict()
+        assert isinstance(animal_dict, dict)
+        assert 'name' in animal_dict
+        assert 'species' in animal_dict
+        assert 'zookeeper' in animal_dict
+        assert 'enclosure' in animal_dict
